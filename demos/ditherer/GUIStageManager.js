9ed3d00964d6f7107bb3353fc01dd384724c9ce8
Josiah Cooksey <jcsoftwaretests@gmail.com>
2025-11-17 22:22:39 -0600

    changes UI instructions based on current stage

--- demos/ditherer/GUIStageManager.js
+++ demos/ditherer/GUIStageManager.js
@@ -20,7 +20,7 @@ class GUIStageManager
     {
         // may not even need to use this first line given we're using events/callbacks
         this.stepHandler = stepHandler;
-        this.buttons = navButtons;
+        this.navButtons = navButtons;
         this.stepHandler.eventHandler.on("stepChange", this.handleStepChange.bind(this));
     }
 
@@ -32,39 +32,44 @@ class GUIStageManager
     handleStepChange(stepData)
     {
         console.log(`step changed to ${stepData.step}; lastStep changed to ${stepData.lastStep}`);
+        document.getElementById("stepDisplay").textContent = `STEP ${stepData.step + 1}/3`;
+        
+        document.querySelectorAll("[data-group-stage]:not(.hidden)").hidden = true;
         switch (stepData.step)
         {
             case STAGES.INPUTIMAGE:
+                document.getElementById("stepInstruction").textContent = "Choose image source";
                 this.showStageGroup(STAGENAMES[STAGES.INPUTIMAGE]);
                 if (stepData.lastStep == STAGES.PALETTE)
                 {
-                    this.buttons[0].hidden = true;
+                    this.navButtons[0].hidden = true;
                 }
                 this.hideStageGroup(STAGENAMES[STAGES.PALETTE]);
-                // document.querySelectorAll("[data-group-stage]:not(.hidden)").hidden = true;
                 break;
 
             case STAGES.PALETTE:
+                document.getElementById("stepInstruction").textContent = "Choose palette source";
                 this.showStageGroup(STAGENAMES[STAGES.PALETTE]);
                 if (stepData.lastStep == STAGES.INPUTIMAGE)
                 {
-                    this.buttons[0].hidden = false;
+                    this.navButtons[0].hidden = false;
                 }
                 else if (stepData.lastStep == STAGES.KERNEL)
                 {
-                    this.buttons[1].hidden = false;
-                    this.buttons[2].hidden = true;
+                    this.navButtons[1].hidden = false;
+                    this.navButtons[2].hidden = true;
                 }
                 this.hideStageGroup(STAGENAMES[stepData.lastStep]);
                 break;
 
             case STAGES.KERNEL:
+                document.getElementById("stepInstruction").textContent = "Choose pattern source";
                 this.showStageGroup(STAGENAMES[STAGES.KERNEL]);
                 if (stepData.lastStep == STAGES.PALETTE)
                 {
-                    this.buttons[1].hidden = true;
+                    this.navButtons[1].hidden = true;
                 }
-                this.buttons[2].hidden = false;
+                this.navButtons[2].hidden = false;
                 this.hideStageGroup(STAGENAMES[stepData.lastStep]);
                 break;
         }
@@ -85,5 +90,7 @@ class GUIStageManager
 }
 
 let stageNavigationButtons = document.querySelectorAll("[data-stage-aspect='navButton']");
-let stepHandler = new GUIStepHandler(stageNavigationButtons[0], stageNavigationButtons[1], 0, 4, false);
+let startingStepIndex = 0;
+let totalStepCount = 4;
+let stepHandler = new GUIStepHandler(stageNavigationButtons[0], stageNavigationButtons[1], startingStepIndex, totalStepCount, false);
 let stageManager = new GUIStageManager(stepHandler, stageNavigationButtons);
