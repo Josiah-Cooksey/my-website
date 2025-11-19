477a7c4a8aba872ae25086402166e595a105ee56
Josiah Cooksey <jcsoftwaretests@gmail.com>
2025-11-19 15:37:56 -0600

    removes unnecessary console logs

--- demos/ditherer/radioHiders.js
+++ demos/ditherer/radioHiders.js
@@ -4,12 +4,10 @@
 let radioHiders = document.querySelectorAll("[data-radio-hider]");
 radioHiders.forEach(radio => {
     radio.addEventListener("click", () => document.querySelectorAll(radio.dataset.radioEnableTarget).forEach(targetElement => {
-        console.log(`showing element ${targetElement}`);
         delete targetElement.dataset.defaultHidden;
         targetElement.hidden = false;
     }));
     radio.addEventListener("click", () => document.querySelectorAll(radio.dataset.radioDisableTarget).forEach(targetElement => {
-        console.log(`hiding element ${targetElement}`);
         targetElement.dataset.defaultHidden = "true";
         targetElement.hidden = true;
     }));
