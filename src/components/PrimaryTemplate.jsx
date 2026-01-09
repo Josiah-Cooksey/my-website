a5fc5bfdc6665220b6c0fd8afe7ef6f3d4a3c552
Josiah Cooksey <jcsoftwaretests@gmail.com>
2026-01-09 16:31:49 -0600

    re-adds error field

--- src/components/PrimaryTemplate.jsx
+++ src/components/PrimaryTemplate.jsx
@@ -17,6 +17,11 @@ export default function PrimaryTemplate({ children, headerPostAddition })
             <body className="p-2 bg-gray-400 dark:bg-gray-900 text-gray-800 dark:text-gray-200 min-h-screen">
                 <header>
                     <button id="darkModeToggle" className="bg-blue-500 m-2 hover:bg-blue-700 active:bg-blue-800 text-white font-bold py-2 px-4 rounded-full">Toggle Dark Mode</button>
+                    <div id="errorField" class="fixed top-0 left-0 z-50 text-center bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded w-full" role="alert" hidden>
+                        <strong class="font-bold text-center block">Error:</strong>
+                        <span id="errorTextField" class="text-center block">Something went wrong, please try again.</span>
+                        <button class="align-right text-gray-500 hover:text-gray-700 font-bold text-xl" onClick={() => document.getElementById('errorField').hidden = true}>&times; Close</button>
+                    </div>
                     { headerPostAddition }
                 </header>
                 <main>{ children }</main>
